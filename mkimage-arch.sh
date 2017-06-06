61f3027ce7e7466334836b3fe62a91e12b76c14e
Zachary Huff <zach.huff.386@gmail.com>
2017-06-05 23:44:24 -0400

    Update mkimage

--- mkimage-arch.sh
+++ mkimage-arch.sh
@@ -63,7 +63,7 @@ expect <<EOF
         exp_send -s -- \$arg
     }
     set timeout 60
-    spawn pacstrap -C ./mkimage-arch-pacman.conf -c -d -G -i $ROOTFS base haveged --ignore $PKGIGNORE
+    spawn pacstrap -C ./mkimage-arch-pacman.conf -c -d -G -i $ROOTFS base haveged libusb --ignore $PKGIGNORE
     expect {
         -exact "anyway? \[Y/n\] " { send -- "n\r"; exp_continue }
         -exact "(default=all): " { send -- "\r"; exp_continue }
@@ -73,7 +73,6 @@ EOF
 
 arch-chroot $ROOTFS /bin/sh -c 'rm -r /usr/share/man/*'
 arch-chroot $ROOTFS /bin/sh -c "haveged -w 1024; pacman-key --init; pkill haveged; pacman -Rs --noconfirm haveged; pacman-key --populate archlinux; pkill gpg-agent"
-arch-chroot $ROOTFS /bin/sh -c "ln -s /usr/share/zoneinfo/UTC /etc/localtime"
 echo 'en_US.UTF-8 UTF-8' > $ROOTFS/etc/locale.gen
 arch-chroot $ROOTFS locale-gen
 arch-chroot $ROOTFS /bin/sh -c 'echo "Server = http://mirror.pritunl.com/archlinux/'$REPO_DATE'/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist'
